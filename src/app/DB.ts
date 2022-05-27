6662a60dea90e9abd85437df9c23af2784ae14ff
Tomás Dominguez <tomyd013004@gmail.com>
2022-05-26 21:35:01 -0300

    primera version apartado educacion

--- src/app/DB.ts
+++ src/app/DB.ts
@@ -16,7 +16,37 @@ export const Educaciones: Educacion[] = [
         instituto: [
             {
                 nombre: "Universidad Nacional de Mar del Plata",
-                logo: "./assets/logoUNMDP.jpg"
+                logo: "./assets/logoUNMDP.png"
+            }
+        ]
+    },
+    {
+        nombre: "Pasantía de investigación en el tema 'Dinámica y control de procesos catastróficos en redes complejas interdependientes y multicapas'",
+        periodo: "2021",
+        instituto: [
+            {
+                nombre: "Universidad Nacional de Mar del Plata",
+                logo: "./assets/logoUNMDP.png"
+            }
+        ]
+    },
+    {
+        nombre: "Pasantía de investigación en el tema 'Propagación y mitigación de procesos epidémicos y cascada de fallas'",
+        periodo: "2021",
+        instituto: [
+            {
+                nombre: "Universidad Nacional de Mar del Plata",
+                logo: "./assets/logoUNMDP.png"
+            }
+        ]
+    },
+    {
+        nombre: "Licenciatura en Física",
+        periodo: "Actual",
+        instituto: [
+            {
+                nombre: "Instituto Balseiro",
+                logo: "./assets/logoBalseiro.png" 
             }
         ]
     }
